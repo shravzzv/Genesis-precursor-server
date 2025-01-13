d75fddf09749c5033de3865db92f1c9d42f5d209
sai shravan <saishravan384@gmail.com>
2025-01-13 18:26:37 +0530

    feat: add endpoint to retrieve a single journal by ID with 404 error handling

--- controllers/journalController.js
+++ controllers/journalController.js
@@ -5,3 +5,12 @@ exports.getAll = asyncHandler(async (req, res) => {
   const journals = await Journal.find()
   res.json(journals)
 })
+
+exports.getOne = asyncHandler(async (req, res) => {
+  const journal = await Journal.findOne({ _id: req.params.id })
+  if (journal) {
+    res.json(journal)
+  } else {
+    res.status(404).json({ message: 'Journal not found.' })
+  }
+})
