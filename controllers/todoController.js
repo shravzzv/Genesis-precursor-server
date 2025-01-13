e8955926256d00ea5842d7cf9cc9449f6a6d3b52
sai shravan <saishravan384@gmail.com>
2025-01-13 14:42:07 +0530

    feat: add endpoint to retrieve a single todo by ID with 404 error handling

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -5,3 +5,12 @@ exports.getAll = asyncHandler(async (req, res) => {
   const todos = await Todo.find()
   res.json(todos)
 })
+
+exports.getOne = asyncHandler(async (req, res) => {
+  const todo = await Todo.findOne({ _id: req.params.id })
+  if (todo) {
+    res.json(todo)
+  } else {
+    res.status(404).json({ message: 'Todo not found.' })
+  }
+})
