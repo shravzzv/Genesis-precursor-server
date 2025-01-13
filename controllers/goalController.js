0f8557e06c1ca3d29dea6d08f3a8e9049660eec7
sai shravan <saishravan384@gmail.com>
2025-01-13 09:58:56 +0530

    feat: add goal creation with validation and user association

--- controllers/goalController.js
+++ controllers/goalController.js
@@ -1,7 +1,35 @@
 const asyncHandler = require('express-async-handler')
 const Goal = require('../models/goal')
+const { body, validationResult, matchedData } = require('express-validator')
 
 exports.getAll = asyncHandler(async (req, res) => {
-  const goals = await Goal.find()
+  const goals = await Goal.find({ user: req.user.id })
   res.json(goals)
 })
+
+exports.create = [
+  body('name')
+    .trim()
+    .notEmpty()
+    .withMessage('Name must not be empty.')
+    .escape(),
+
+  asyncHandler(async (req, res) => {
+    const errors = validationResult(req)
+    const { name } = matchedData(req, {
+      onlyValidData: false,
+    })
+
+    const newGoal = new Goal({
+      name,
+      user: req.user.id,
+    })
+
+    if (errors.isEmpty()) {
+      await newGoal.save()
+      res.json({ newGoal })
+    } else {
+      res.status(401).json(errors.array())
+    }
+  }),
+]
--- routes/goal.js
+++ routes/goal.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const goalController = require('../controllers/goalController')
+const protect = require('../middleware/protect.middleware')
+
+router.use(protect)
 
 router.get('/', goalController.getAll)
+
+router.post('/', goalController.create)
 
 module.exports = router
