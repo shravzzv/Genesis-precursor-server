5603b08d88cd526969ad09788422b16cb92f0827
sai shravan <saishravan384@gmail.com>
2025-01-13 15:42:06 +0530

    feat: add endpoint to retrieve a single habit by ID with 404 error handling

--- controllers/habitController.js
+++ controllers/habitController.js
@@ -1,7 +1,17 @@
 const asyncHandler = require('express-async-handler')
 const Habit = require('../models/habit')
+const { body, validationResult, matchedData } = require('express-validator')
 
 exports.getAll = asyncHandler(async (req, res) => {
   const habits = await Habit.find()
   res.json(habits)
+})
+
+exports.getOne = asyncHandler(async (req, res) => {
+  const habit = await Habit.findOne({ _id: req.params.id })
+  if (habit) {
+    res.json(habit)
+  } else {
+    res.status(404).json({ message: 'Habit not found.' })
+  }
 })
